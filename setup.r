ce224f0322bec4403a69fc7c209bfa4146160b2a
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-11-21 16:00:35 -0500

    Install and set up packages

---
+++ setup.r
@@ -0,0 +1,16 @@
+# Packages to install
+to_install <- c("ggplot2", "plyr", "ggmap", "knitr", "maps")
+
+# Looking at an object
+str(to_install)
+
+# Installing packages
+install.packages(to_install)
+
+# Removing an object
+rm(to_install)
+
+library(ggplot2)
+library(ggmap)
+library(plyr)
+library(maps)

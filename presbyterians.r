b2f578719107b0f598e9fa3287e096ba1c81cde0
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-11-21 18:30:48 -0500

    Sample plots of Presbyterians

---
+++ presbyterians.r
@@ -0,0 +1,46 @@
+Presbyterian data
+
+```{r}
+library(ggplot2)
+library(plyr)
+source("presbyterian-load.r")
+```
+
+Let's plot the membership of the denomination:
+
+```{r}
+ggplot(pres) +
+  geom_point(aes(x = year, y = membership))
+```
+
+We have a problem, which is that there was a denominational split. We need a way to distinguish between the denominations but to represent them together.
+
+```{r}
+ggplot(pres,
+       aes(x = year, y = membership, fill = denomination)) +
+  geom_area() 
+```
+
+What was the trend of infant baptism?
+
+```{r}
+
+```
+
+What was the trend of infant baptism relative to population?
+
+```{r}
+
+```
+
+What was the trend of infant baptism relative to all baptisms?
+
+```{r}
+
+```
+
+What was the trend of infant baptism relative to all ways of entering the Presbyterian church?
+
+```{r}
+
+```
